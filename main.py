32decaa0ebbb77459d696c7da2e94ff0091933f4
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-04-30 12:00:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,29 @@ st.title("Parcelles en Agriculture Biologique déclarées à la PAC")
 
 # Charger les données à partir de l'URL
 url = "https://static.data.gouv.fr/resources/parcelles-en-agriculture-biologique-ab-declarees-a-la-pac/20221122-124454/rpg-bio-2021-national.shp.zip"
+
+# Ajouter un indicateur de progression
+progress_bar = st.progress(0)
+progress_text = st.empty()
+
+progress_text.text("Téléchargement des données...")
 response = requests.get(url)
+progress_bar.progress(25)
 
 with tempfile.TemporaryDirectory() as tmpdirname:
+    progress_text.text("Extraction des fichiers...")
     with ZipFile(BytesIO(response.content)) as zf:
         zf.extractall(tmpdirname)
-    
+    progress_bar.progress(50)
+
+    progress_text.text("Chargement du fichier shapefile...")
     shp_path = [os.path.join(tmpdirname, file) for file in os.listdir(tmpdirname) if file.endswith(".shp")][0]
     shp = gpd.read_file(shp_path)
+    progress_bar.progress(100)
+
+# Supprimer l'indicateur de progression une fois les données chargées
+progress_bar.empty()
+progress_text.empty()
 
 # Afficher un extrait des données
 st.write(shp.head())
