17e456b8f30d89f6fa31558564aed713f3f21cc8
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-05-01 14:05:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
 import streamlit as st
-import geopandas as gpd
-import requests
 from io import BytesIO
+from urllib.request import urlopen
 from zipfile import ZipFile
-import tempfile
-import os
-import folium
-from streamlit_folium import folium_static
 
-st.title("Parcelles en Agriculture Biologique déclarées à la PAC")
+def download_unzip(zipurl, destination):
+    """Download zipfile from URL and extract it to destination"""
+    with urlopen(zipurl) as zipresp:
+        with ZipFile(BytesIO(zipresp.read())) as zfile:
+            zfile.extractall(destination)
 
-# Charger les données à partir de l'URL
-url = "https://static.data.gouv.fr/resources/parcelles-en-agriculture-biologique-ab-declarees-a-la-pac/20221122-124454/rpg-bio-2021-national.shp.zip"
+def main():
+    st.title("Dataviz sur Streamlit")
 
-# Ajouter un indicateur de progression
-progress_bar = st.progress(0)
-progress_text = st.empty()
+    st.header("Téléchargement et extraction des fichiers de données")
+    for decade in 1970, 1980, 1990, 2000, 2010:
+        url = f"https://www.insee.fr/fr/statistiques/fichier/4769950/deces-{decade}-{decade + 9}-csv.zip"
+        st.write("Downloading and extracting", url)
+        download_unzip(url, 'data')
 
-progress_text.text("Téléchargement des données...")
-response = requests.get(url)
-progress_bar.progress(25)
+    for year in 2020, 2023:
+        url = f"https://www.insee.fr/fr/statistiques/fichier/4190491/Deces_{year}.zip"
+        st.write("Downloading and extracting", url)
+        download_unzip(url, 'data')
 
-with tempfile.TemporaryDirectory() as tmpdirname:
-    progress_text.text("Extraction des fichiers...")
-    with ZipFile(BytesIO(response.content)) as zf:
-        zf.extractall(tmpdirname)
-    progress_bar.progress(50)
+    st.success("Téléchargement et extraction des fichiers de données terminés.")
 
-    progress_text.text("Chargement du fichier shapefile...")
-    shp_path = [os.path.join(tmpdirname, file) for file in os.listdir(tmpdirname) if file.endswith(".shp")][0]
-    shp = gpd.read_file(shp_path)
-    progress_bar.progress(100)
-
-# Supprimer l'indicateur de progression une fois les données chargées
-progress_bar.empty()
-progress_text.empty()
-
-# Afficher un extrait des données
-st.write(shp.head())
-
-# Afficher les données sur une carte
-m = folium.Map(location=[47.0833, 2.4000], zoom_start=6)
-shp_json = shp.to_crs(epsg=4326).to_json()
-
-folium.GeoJson(shp_json).add_to(m)
-folium_static(m)
+if __name__ == "__main__":
+    main()
