ba88029341327fa8b3495212428e19a2d9f6ad75
EL HASSANI Samir <115094184+samirelhassani1998@users.noreply.github.com>
2023-05-01 14:18:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import streamlit as st
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
 import pandas as pd
 import numpy as np
 import glob
+
+def download_unzip(zipurl, destination):
+    """Download zipfile from URL and extract it to destination"""
+    with urlopen(zipurl) as zipresp:
+        with ZipFile(BytesIO(zipresp.read())) as zfile:
+            zfile.extractall(destination)
 
 def load_data():
     csv_files = sorted(glob.glob('data/*.csv'))
@@ -21,12 +31,20 @@ def load_data():
     del df_years  # free memory
     return df
 
-# Modifiez la fonction main() pour inclure le chargement des données et afficher un aperçu du DataFrame
 def main():
     st.title("Dataviz sur Streamlit")
 
     st.header("Téléchargement et extraction des fichiers de données")
-    # ...
+    for decade in 1970, 1980, 1990, 2000, 2010:
+        url = f"https://www.insee.fr/fr/statistiques/fichier/4769950/deces-{decade}-{decade + 9}-csv.zip"
+        st.write("Downloading and extracting", url)
+        download_unzip(url, 'data')
+
+    for year in 2020, 2023:
+        url = f"https://www.insee.fr/fr/statistiques/fichier/4190491/Deces_{year}.zip"
+        st.write("Downloading and extracting", url)
+        download_unzip(url, 'data')
+
     st.success("Téléchargement et extraction des fichiers de données terminés.")
 
     st.header("Chargement des données")
